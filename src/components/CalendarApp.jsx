018e41f6dd9f672daa49d4db460fe4f666df25c1
Sreenathselva <sreenathselva0@gmail.com>
2025-06-02 16:26:36 +0530

    updated some more functionalities like adding events on click to specific dates.

--- src/components/CalendarApp.jsx
+++ src/components/CalendarApp.jsx
@@ -19,26 +19,86 @@ const CalendarApp = () => {
     "December"
   ];
 
-const currentDate = new Date()
+  const currentDate = new Date()
 
-const [currentMonth, setCurrentMonth] = useState(currentDate.getMonth());
-const [currentYear, setCurrentYear] = useState(currentDate.getFullYear());
+  const [currentMonth, setCurrentMonth] = useState(currentDate.getMonth());
+  const [currentYear, setCurrentYear] = useState(currentDate.getFullYear());
+  const [selectedDate, setSelectedDate] = useState(currentDate);
+  const [showEventPopup, setShowEventPopup] = useState(false);
+  const [events,setEvents] = useState([])
+  const [eventTime, setEventTime] = useState({hours: '00', minutes: '00'})
+  const [eventText, setEventText] = useState('')
+  const [editingEvent, setEditingEvent] = useState(null);
 
-const daysInMonth = new Date(currentYear, currentMonth + 1, 0).getDate();
-const firstDayOfMonth = new Date(currentYear,currentMonth, 1).getDay();
+  const daysInMonth = new Date(currentYear, currentMonth + 1, 0).getDate();
+  const firstDayOfMonth = new Date(currentYear, currentMonth, 1).getDay();
 
-console.log(currentYear)
+  const prevMonth = () => {
+    setCurrentMonth((prevMonth) => (prevMonth === 0 ? 11 : prevMonth - 1))
+    setCurrentYear((prevYear) => currentMonth === 0 ? prevYear - 1 : prevYear)
+  }
 
-const prevMonth = () => {
-  setCurrentMonth((prevMonth) => (prevMonth === 0 ? 11 : prevMonth - 1))
-  setCurrentYear((prevYear) => currentMonth === 0 ? prevYear - 1 : prevYear)
-}
+  const nextMonth = () => {
+    setCurrentMonth((prevMonth) => (prevMonth === 11 ? 0 : prevMonth + 1))
+    setCurrentYear((prevYear) => currentMonth === 11 ? prevYear + 1 : prevYear)
+  }
 
-const nextMonth = () => {
-  setCurrentMonth((prevMonth) => (prevMonth === 11 ? 0 : prevMonth + 1))
-  setCurrentYear((prevYear) => currentMonth === 11 ? prevYear + 1 : prevYear)
-}
+  const handleDayClick = (day) => {
+    const clickedDate = new Date(currentYear, currentMonth, day)
+    const today = new Date()
 
+    if(clickedDate >= today || isSameDay(clickedDate, today)){
+      setSelectedDate(clickedDate)
+      setShowEventPopup(true);
+      setEventText("")
+      setEventTime({hours: '00', minutes: '00'})
+      setEditingEvent(null)
+    }
+  }
+
+  const isSameDay = (date1, date2) => {
+    return(
+      date1.getFullYear() === date2.getFullYear() || date1.getFullYear() < date1.getFullYear() && 
+      date1.getMonth() === date2.getMonth() || date1.getMonth() < date1.getMonth() && 
+      date1.getDate() === date2.getDate() || date1.getDate() < date1.getDate()
+    )
+  }
+
+  const handleEventSubmit = () => {
+    const newEvent = {
+      id: editingEvent ? editingEvent.id : Date.now(),
+      date: selectedDate,
+     time: `${eventTime.hours.padStart(2, '0')}:${eventTime.minutes.padStart(2, '0')}`,
+      text: eventText,
+    }
+
+    let updatedEvents= [...events]
+
+    if(editingEvent){
+      updatedEvents = udpatedEvents.map((events) => event.id === editingEvent.id ? newEvent : event,
+    )} else{
+      updatedEvents.push(newEvent)
+    }
+
+    updatedEvents.sort((a, b)=> new Date(a.date - new Date(b.date)))
+
+    setEvents([...events,newEvent])
+    setEventTime({hours: '00', minutes: '00'})
+    setEventText("")
+    setShowEventPopup(false)
+    setEditingEvent(null);
+  } 
+
+  const handleEditEvent = (event) => {
+    setSelectedDate[new Date(event.date)]
+    setEventTime({
+      hours: event.time.split(":")[0],
+      minutes: event.time.split(":")[1]
+    })
+    setEventText(event,text)
+    setEditingEvent(event)
+    setShowEventPopup(true)
+  }
 
 
   return (
@@ -57,37 +117,61 @@ const nextMonth = () => {
           {daysOfWeek.map((day) => <span key={day}>{day}</span>)}
         </div>
         <div className="days">
-          {[...Array(firstDayOfMonth).keys()].map((_, index) =>(
-             <span key={`empty-${index}`}/>
+          {[...Array(firstDayOfMonth).keys()].map((_, index) => (
+            <span key={`empty-${index}`} />
           ))}
           {[...Array(daysInMonth).keys()].map((day) =>
-             <span key={day+1}>{day+1}</span>
-             )}
+            <span key={day + 1} className={
+              day + 1 === currentDate.getDate() 
+              && currentMonth === currentDate.getMonth() 
+              && currentYear === currentDate.getFullYear() 
+              ? 'current-day' : '' }
+              onClick={()=> handleDayClick(day + 1)}
+              >{day + 1}</span>
+          )}
         </div>
       </div>
       <div className="events">
+        {showEventPopup && 
         <div className="event-popup">
           <div className="time-input">
             <div className="event-popup-time">Time</div>
-            <input type="number" name="hours" min={0} max={24} className="hours" />
-            <input type="number" name="minutes" min={0} max={60} className="minutes" />
+            <input type="number" name="hours" min={0} max={24} className="hours" 
+            value={eventTime.hours} 
+            onChange={(e) =>
+              setEventTime({...eventTime, hours: e.target.value })
+            } />
+            <input type="number" name="minutes" min={0} max={60} className="minutes"  
+            value={eventTime.minutes} 
+            onChange={(e) =>
+              setEventTime({...eventTime, minutes: e.target.value })
+            }/>
           </div>
-          <textarea placeholder="Enter Event Text (Maximum 60 Characters)" name="" id=""></textarea>
-          <button className="event-popup-btn">Add Event</button>
-          <button className="close-event-popup">
+          <textarea placeholder="Enter Event Text (Maximum 60 Characters)"
+          value={eventText} onChange={(e) => {
+            if(e.target.value.length <= 60){
+              setEventText(e.target.value)
+            }
+          }}
+           name="" id=""></textarea>
+          <button className="event-popup-btn" onClick={handleEventSubmit}>Add Event</button>
+          <button className="close-event-popup" onClick={()=> setShowEventPopup(false)}>
             <span>x</span>
           </button>
-        </div>
-        <div className="event">
+        </div>}
+        {events.map((event,  index) => (
+          
+        <div className="event" key={index}>
           <div className="event-date-wrapper">
-            <div className="event-date">May 15, 2024</div>
-            <div className="event-time">10:00</div>
+            <div className="event-date">{`${monthOfYear[event.date.getMonth()]} ${event.date.getFullYear()}`}</div>
+            <div className="event-time">{event.time}</div>
           </div>
-          <div className="event-text">ThaiCyberX</div>
+          <div className="event-text">{event.text}</div>
           <div className="event-buttons">
-          </div>
 
+          </div>
         </div>
+        ))}
       </div>
     </div>
   );
